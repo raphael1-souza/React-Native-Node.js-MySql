11b2b69e1e6051ef000d0646dd4dfa5ab1e6a8be
Raphael <raphasilvasouza96@gmail.com>
2019-01-13 21:45:48 -0300

    servidor

---
+++ main.js
@@ -0,0 +1,14 @@
+var metodos = require('./methods');
+
+
+// metodos.post(
+//     "Jorge",
+//     "Oliveira",
+//     "Descricao"
+// );
+
+metodos.get();
+
+// metodos.put(3, "Jorge", "Oliveira", "Descricao");
+
+// metodos.delete(3);
